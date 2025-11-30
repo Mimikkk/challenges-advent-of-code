9d3e0b96732c1fd9458eabe2ebbe8fe34eb15ea0
Mimikkk <zdancewicz.daniel@gmail.com>
2025-11-30 23:38:07 +0100

    day 5 p1

--- languages/typescript/src/2023/day-05/day-05.test.ts
+++ languages/typescript/src/2023/day-05/day-05.test.ts
@@ -1,7 +1,8 @@
-
-import { createPuzzleTest } from "../../utils/create-puzzle-test.ts";
-import puzzle from "./day-05.ts";
+import { createPuzzleTest } from '../../utils/create-puzzle-test.ts';
+import puzzle from './day-05.ts';
 
 createPuzzleTest({
   puzzle,
+  easyTest: 35,
+  easyUser: 199602917,
 });
--- languages/typescript/src/2023/day-05/day-05.ts
+++ languages/typescript/src/2023/day-05/day-05.ts
@@ -1,9 +1,66 @@
+import { Puzzle } from '../../types/puzzle.ts';
 
-import { Puzzle } from "../../types/puzzle.ts";
-import { Str } from "../../utils/strs.ts";
+type Range = { offset: number; start: number; size: number };
+
+const mapRange = (source: number, range: Range): number => {
+  if (source >= range.start && source < range.start + range.size) {
+    return range.offset + (source - range.start);
+  }
+  return source;
+};
+
+const mapRanges = (
+  source: number,
+  ranges: Range[],
+): number => {
+  for (const range of ranges) {
+    if (source >= range.start && source < range.start + range.size) {
+      return range.offset + (source - range.start);
+    }
+  }
+
+  return source;
+};
+
+type Category = 'seed' | 'soil' | 'fertilizer' | 'water' | 'light' | 'temperature' | 'humidity' | 'location';
+const order: Category[] = ['soil', 'fertilizer', 'water', 'light', 'temperature', 'humidity', 'location'];
+const parseMaps = (input: string) => {
+  const [[seedsStr], ...mapsStrs] = input.split('\n\n').map((s) => s.trim().split('\n'));
+
+  const matchDirections = (str: string) => str.match(/(\w+)-to-(\w+)/)!.slice(1);
+  const extractNumbers = (str: string) => str.matchAll(/\d+/g).map((match) => +match[0]).toArray();
+  const seeds = extractNumbers(seedsStr);
+
+  const maps = new Map<Category, (source: number) => number>(mapsStrs.map(([name, ...records]) => {
+    const [_, to] = matchDirections(name);
+
+    const ranges = records.map(extractNumbers).map(([offset, start, size]) => ({ offset, start, size }));
+
+    return [
+      to,
+      (source) => mapRanges(source, ranges),
+    ] as [Category, (source: number) => number];
+  }));
+
+  return { seeds, maps };
+};
 
 export default Puzzle.new({
-  prepare: Str.lines,
-  easy: () => 0,
+  prepare: (input) => parseMaps(input),
+  easy: ({ seeds, maps }) => {
+    let min = Infinity;
+
+    for (let seed of seeds) {
+      for (const category of order) {
+        seed = maps.get(category)!(seed);
+      }
+
+      if (seed < min) {
+        min = seed;
+      }
+    }
+
+    return min;
+  },
   hard: () => 0,
 });
