434eb2eecb124eeaa1285c23d8686d16fc207e21
Mimikkk <zdancewicz.daniel@gmail.com>
2025-11-30 20:45:18 +0100

    2023 day 2

--- languages/typescript/src/2023/day-02/day-02.test.ts
+++ languages/typescript/src/2023/day-02/day-02.test.ts
@@ -1,7 +1,10 @@
-
-import { createPuzzleTest } from "../../utils/create-puzzle-test.ts";
-import puzzle from "./day-02.ts";
+import { createPuzzleTest } from '../../utils/create-puzzle-test.ts';
+import puzzle from './day-02.ts';
 
 createPuzzleTest({
   puzzle,
+  // easyTest: 8,
+  // easyUser: 2476,
+  hardTest: 2286,
+  // hardUser: 54911,
 });
--- languages/typescript/src/2023/day-02/day-02.ts
+++ languages/typescript/src/2023/day-02/day-02.ts
@@ -1,9 +1,56 @@
+import { Puzzle } from '../../types/puzzle.ts';
+import { sum } from '../../utils/maths.ts';
+import { Str } from '../../utils/strs.ts';
 
-import { Puzzle } from "../../types/puzzle.ts";
-import { Str } from "../../utils/strs.ts";
+type CubeType = 'blue' | 'green' | 'red';
+type CubeCountRecord = Record<CubeType, number>;
+type Game = Partial<CubeCountRecord>[];
+
+const parseGames = (gameStrs: string[]): Game[] =>
+  gameStrs.map((gameStr) =>
+    gameStr.split(':')[1].split(';').map((roundStr) =>
+      Object.fromEntries(
+        roundStr
+          .split(',')
+          .map((value) => value.split(' ').filter(Boolean))
+          .map(([count, type]) => [type, +count]),
+      )
+    )
+  ) as Game[];
+
+const isGamePossible = (game: Game): boolean => {
+  for (const { red, green, blue } of game) {
+    if (red && red > 12) return false;
+    if (green && green > 13) return false;
+    if (blue && blue > 14) return false;
+  }
+
+  return true;
+};
+
+const findGameLeastCubeCounts = (game: Game): CubeCountRecord => {
+  const result = { red: 0, green: 0, blue: 0 };
+
+  for (const { red, green, blue } of game) {
+    if (red && red > result.red) result.red = red;
+    if (green && green > result.green) result.green = green;
+    if (blue && blue > result.blue) result.blue = blue;
+  }
+
+  return result;
+};
+
+const findGamePower = (game: Game): number => {
+  const { blue, green, red } = findGameLeastCubeCounts(game);
+  return blue * green * red;
+};
 
 export default Puzzle.new({
   prepare: Str.lines,
-  easy: () => 0,
-  hard: () => 0,
+  easy(strs) {
+    return sum(parseGames(strs).map((game, i) => isGamePossible(game) ? i + 1 : 0));
+  },
+  hard(strs) {
+    return sum(parseGames(strs).map(findGamePower));
+  },
 });
