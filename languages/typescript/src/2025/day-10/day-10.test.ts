23d85648754f7c2ca2f64a62ffa93dab60e3bea6
Mimikkk <zdancewicz.daniel@gmail.com>
2025-12-10 23:26:16 +0100

    day 10 p12 lpscam

---
+++ languages/typescript/src/2025/day-10/day-10.test.ts
@@ -0,0 +1,10 @@
+import { createPuzzleTest } from '../../utils/create-puzzle-test.ts';
+import puzzle from './day-10.ts';
+
+createPuzzleTest({
+  puzzle,
+  // easyTest: 7,
+  // easyUser: 466,
+  hardTest: 33,
+  hardUser: 17214,
+});
